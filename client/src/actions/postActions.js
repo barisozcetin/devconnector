bf8277fccd59000ec03b538f7f813ebd18fadd80
baris_000 <barisozcetin@gmail.com>
2018-05-10 14:35:06 +0300

    get posts actions/reducers

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-import { ADD_POST, GET_ERRORS } from "./types";
+import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from "./types";
 
+// Add Post
 export const addPost = postData => dispatch => {
   axios
     .post("/api/posts", postData)
@@ -17,3 +18,27 @@ export const addPost = postData => dispatch => {
       })
     );
 };
+
+// Get posts
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get("/api/posts")
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+// Set loading state
+export const setPostLoading = () => ({
+  type: POST_LOADING
+});
