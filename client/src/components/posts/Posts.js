83a02d90fd2ddc60f4adcd775798848631be6f93
baris_000 <barisozcetin@gmail.com>
2018-05-10 14:35:16 +0300

    Post feed started

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,17 +3,35 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import PostForm from "./PostForm";
 import Spinner from "../common/Spinner";
+import { getPosts } from "../../actions/postActions";
+import PostFeed from "./PostFeed";
 
 class Posts extends Component {
-  static propTypes = {};
+  static propTypes = {
+    post: PropTypes.object.isRequired
+  };
+
+  componentDidMount() {
+    this.props.getPosts();
+  }
 
   render() {
+    const { posts, loading } = this.props.post;
+    let postContent;
+
+    if (posts === null || loading) {
+      postContent = <Spinner />;
+    } else {
+      postContent = <PostFeed posts={posts} />;
+    }
+
     return (
       <div className="feed">
         <div className="container">
           <div className="row">
             <div className="col-md-12">
               <PostForm />
+              {postContent}
             </div>
           </div>
         </div>
@@ -22,4 +40,9 @@ class Posts extends Component {
   }
 }
 
-export default Posts;
+const mapStateToProps = state => ({
+  post: state.post,
+  getPosts: PropTypes.func.isRequired
+});
+
+export default connect(mapStateToProps, { getPosts })(Posts);
