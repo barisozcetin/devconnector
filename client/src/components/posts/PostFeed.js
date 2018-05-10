6ce1ab504a400a2e298f46beab88f5b7cf9fa4a6
baris_000 <barisozcetin@gmail.com>
2018-05-10 15:02:53 +0300

    Post feed started working

--- client/src/components/posts/PostFeed.js
+++ client/src/components/posts/PostFeed.js
@@ -9,7 +9,7 @@ class PostFeed extends Component {
 
   render() {
     const { posts } = this.props;
-    return posts.map(post => <PostItem key={post._id} post={pst} />);
+    return posts.map(post => <PostItem key={post._id} post={post} />);
   }
 }
 
